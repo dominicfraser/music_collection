407e090d0e5eeb7de5c4822985e68e2fe7ae13dc
Dominic <mrdjfraser@gmail.com>
2017-03-22 15:05:29 +0000

    populated the tables with sample data

--- db/music_collection.sql
+++ db/music_collection.sql
@@ -12,3 +12,16 @@ CREATE TABLE albums (
   title VARCHAR(255),
   genre VARCHAR(255)
 );
+
+INSERT INTO artists (name) VALUES ('artist1');
+INSERT INTO artists (name) VALUES ('artist2');
+INSERT INTO artists (name) VALUES ('artist3');
+INSERT INTO artists (name) VALUES ('artist4');
+INSERT INTO albums (artist_id, title, genre) VALUES (1, 'album1','genre1');
+INSERT INTO albums (artist_id, title, genre) VALUES (2, 'album2','genre2');
+INSERT INTO albums (artist_id, title, genre) VALUES (3, 'album3','genre2');
+INSERT INTO albums (artist_id, title, genre) VALUES (4, 'album4','genre2');
+INSERT INTO albums (artist_id, title, genre) VALUES (4, 'album5','genre1');
+
+-- SELECT * FROM artists;
+-- SELECT * FROM albums;
