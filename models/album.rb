a14e3dea1037807cbe7fa3401c8ca571c28ab405
Dominic <mrdjfraser@gmail.com>
2017-03-22 15:27:10 +0000

    initialize set up

--- models/album.rb
+++ models/album.rb
@@ -3,6 +3,15 @@ require_relative('../db/sql_runner')
 
 class Album
 
+  attr_writer :title, :genre
+
+  def initialize(album_hash)
+    @id = album_hash['id'].to_i
+    @artist_id = album_hash['artist_id'].to_i
+    @title = album_hash['title']
+    @genre = album_hash['genre']
+  end
+
 
 
 
--- models/artist.rb
+++ models/artist.rb
@@ -3,8 +3,12 @@ require_relative('../db/sql_runner')
 
 class Artist
 
+  attr_writer :name
 
-
+  def initialize(artist_hash)
+    @id = artist_hash['id'].to_i
+    @name = artist_hash['name']
+  end
 
 
 end
